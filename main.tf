fa301512d3ef9fab1d40e76b7744e1e22463d6b9
cf <cf@test.com>
2025-09-26 10:59:15 +0100

    initial

---
+++ main.tf
@@ -0,0 +1,11 @@
+terraform {
+    backend "s3" {
+  }
+}
+
+# An example resource that does nothing.
+resource "null_resource" "example" {
+  triggers = {
+    value = "A example resource that does nothing!"
+  }
+}
