3f1ea9eb5a8826d4d282871811bd9a48b0ed6233
cf <cf@test.com>
2025-09-26 15:18:30 +0100

    change backend

--- main.tf
+++ main.tf
@@ -1,15 +1,22 @@
 terraform {
-    backend "remote" {
-        organization = "demo-terraform-actions"
-        workspaces {
-            name = "terraform-actions-test"
+    backend "s3" {
+    endpoints = {
+      s3 = "https://s3.eu-central-003.backblazeb2.com"
     }
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    region                      = "eu-central-003"
+    bucket                      = "bb-eu-s3-private-terraform"
+    key                         = "actions-test/terraform.tfstate"
   }
 }
 
 # An example resource that does nothing.
 resource "null_resource" "example" {
   triggers = {
-    value = "A example resource that does nothing!2"
+    value = "A example resource that does nothing!"
   }
-}
+} 
